2694acfa9ab8f0bb6a00ddc67a6742a37521d77a
handt0311 <trunghieu031124@gmail.com>
2025-09-05 15:38:21 +0700

    update5/9/2025

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT =8888;
+
+// Main route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+// Start server
+app.listen(PORT, () =>{
+    console.log(`Server is starting at http://localhost:${PORT}`);
+})
